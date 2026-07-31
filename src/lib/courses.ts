d5eaeaea098fc0d6f3e830ef999f205213e02b74
J Riteesh Reddy <riteesh4754x@gmail.com>
2026-07-31 22:45:34 +0530

    Update Basketball faculty and docUrl

--- src/lib/courses.ts
+++ src/lib/courses.ts
@@ -9,7 +9,7 @@ export interface Course {
 
 export const COURSES: Course[] = [
   // 8 Sports Courses
-  { id: "SP01", name: "Basketball", category: "Sports", maxSeats: 25, faculty: "<faculty name>", docUrl: "" },
+  { id: "SP01", name: "Basketball", category: "Sports", maxSeats: 25, faculty: "Nagarjun Talawar", docUrl: "https://docs.google.com/document/d/1rgfw08lLhqR6a0hbKbm2bTPGgdq4h_2u/edit?usp=sharing&rtpof=true&sd=true" },
   { id: "SP02", name: "Cricket", category: "Sports", maxSeats: 25, faculty: "<faculty name>", docUrl: "" },
   { id: "SP03", name: "Fitness and Nutrition", category: "Sports", maxSeats: 25, faculty: "<faculty name>", docUrl: "" },
   { id: "SP04", name: "Football", category: "Sports", maxSeats: 25, faculty: "<faculty name>", docUrl: "" },
