3f55b4e86c6b88df0fce06f11b9d83296f7a8fe0
J Riteesh Reddy <riteesh4754x@gmail.com>
2026-07-31 22:49:52 +0530

    Update Kabaddi faculty and docUrl

--- src/lib/courses.ts
+++ src/lib/courses.ts
@@ -13,7 +13,7 @@ export const COURSES: Course[] = [
   { id: "SP02", name: "Cricket", category: "Sports", maxSeats: 25, faculty: "SAINATH C", docUrl: "https://docs.google.com/document/d/19KLq-7IdriQj08nn7CQWxA-lGkuvC5p6/edit?usp=sharing&ouid=117607264633629638910&rtpof=true&sd=true" },
   { id: "SP03", name: "Fitness and Nutrition", category: "Sports", maxSeats: 25, faculty: "Bharath K G", docUrl: "https://docs.google.com/document/d/1ljZ2-zCs0Kp_AWOCxZAl68uyRdV-fSe2/edit?usp=sharing&ouid=117607264633629638910&rtpof=true&sd=true" },
   { id: "SP04", name: "Football", category: "Sports", maxSeats: 25, faculty: "PAGUTHARIVALAN A", docUrl: "https://docs.google.com/document/d/1LfDwz4-Y2ecTt3RZTwKREN5g21ot0z2o/edit?usp=sharing&ouid=117607264633629638910&rtpof=true&sd=true" },
-  { id: "SP05", name: "Kabaddi", category: "Sports", maxSeats: 25, faculty: "<faculty name>", docUrl: "" },
+  { id: "SP05", name: "Kabaddi", category: "Sports", maxSeats: 25, faculty: "Mota Chandranna", docUrl: "https://docs.google.com/document/d/1pNszp6GA707bsFNCiuyni4vjgS6uZZ0X/edit?usp=sharing&ouid=117607264633629638910&rtpof=true&sd=true" },
   { id: "SP06", name: "Holistic Wellbeing and Yoga Therapy", category: "Sports", maxSeats: 25, faculty: "Dr. Prajwala H V", docUrl: "https://docs.google.com/document/d/1d9j62RwwNzfibMjCflUG-JPQX1Q2Ca0y/edit?usp=sharing&ouid=117607264633629638910&rtpof=true&sd=true" },
   { id: "SP07", name: "Throwball", category: "Sports", maxSeats: 25, faculty: "<faculty name>", docUrl: "" },
   { id: "SP08", name: "Volleyball", category: "Sports", maxSeats: 25, faculty: "<faculty name>", docUrl: "" },
